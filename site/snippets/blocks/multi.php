6cabd84ff7d8d4ec7ed2fd937234da44bdaeed0b
Jan Koll <mail@jankoll.de>
2019-09-16 22:46:02 +0200

    Update Builder Config, Add Multiple Accordion Layout, Add Error Page

---
+++ site/snippets/blocks/multi.php
@@ -0,0 +1,24 @@
+<div class="chapter grid-container full" id="<?= $data->_uid() ?>">
+  <div class="grid-container chapter-inner">
+    <div class="grid-x grid-padding-x">
+      <div class="cell medium-offset-4 medium-8 small-12" data-sticky-container>
+        <h2><?= $data->paragraphheadline() ?></h2>
+        <ul class="accordion" data-accordion data-allow-all-closed="true">
+          <?php foreach($data->mybuilder()->toBuilderBlocks() as $multi): ?>
+            <li class="accordion-item" data-accordion-item>
+              <a href="#" class="accordion-title">
+                <div class="media-control">0:00 / 5:24</div>
+                <p><b>Datum:</b> <?= $multi->paragraphMultiDate()->toDate('d.m.Y') ?></p>
+                <p><?= $multi->paragraphMultiTarget() ?></p>
+                <p><?= $multi->paragraphMultiTechnology() ?></p>
+              </a>
+              <div class="accordion-content" data-tab-content>
+                <?= $multi->paragraphMultiText()->kt() ?>
+              </div>
+            </li>
+          <?php endforeach ?>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
--- site/snippets/builder.php
+++ site/snippets/builder.php
@@ -1,5 +1,3 @@
 <?php foreach($page->mybuilder()->toBuilderBlocks() as $block): ?>
-
     <?= snippet('blocks/' . $block->_key(), array('data' => $block)) ?>
-
 <?php endforeach ?>
